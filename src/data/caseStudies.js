e4f3b617a7fb5af811af5c321b8f6e4ec8156ed4
Daniel <danbernardi1@gmail.com>
2017-01-18 09:59:17 -0800

    starts refactors case studies

--- src/data/caseStudies.js
+++ src/data/caseStudies.js
@@ -7,7 +7,27 @@ import CSYumavore from 'routes/Home/Yumavore/CSYumavore';
 export const caseStudies = [
   {
     id: 'norton',
-    component: <CSNorton />
+    content: [
+      {
+        img: norton1,
+        copy: [
+          { classes: 'casestudy__text typ--h3', text: 'All your protected devices are connected in a single, simple interface.' }
+        ]
+      },
+      {
+        img: norton2,
+        copy: [
+          { classes: 'casestudy__text typ--h3', text: 'One central interface gives users access to Norton’s entire product lineup with one-click installation.' },
+          { classes: 'casestudy__text typ--h3', text: 'Discover and deploy Norton on all your connected devices, from anywhere.' }
+        ]
+      },
+      {
+        img: norton4,
+        copy: [
+          { classes: 'casestudy__text typ--h4', text: 'Winner of a 2012 Communicator Award and shortlisted for a 2012 Design Week Award. Norton employees can easily explore patterns in critical data driving the business with one touch.' }
+        ]
+      }
+    ]
   },
   {
     id: 'yumavore',
@@ -22,3 +42,17 @@ export const caseStudies = [
     component: <CSFive />
   }
 ];
+
+/*
+  return (
+    <CaseStudy
+      id="norton"
+      name="Norton Antivirus"
+      heading="We were chosen by Symantec to design a cloud-based solution which allows customers to discover, buy and manage the entire suite of Norton products."
+      content={ content }
+      next={ { id: 'yumavore', name: 'Yumavore', component: <CSYumavore /> } }
+      animateIn={ animateIn }
+    />
+  );
+};
+ */
