f5cf5ba93417c34f3d3b0230de23a89d87667187
Daniel <danbernardi1@gmail.com>
2017-01-17 16:53:24 -0800

    adds placeholder archive case study

---
+++ src/routes/Archives/Archive1/CSArchive1.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import CaseStudy from '../../../components/CaseStudy/index';
+import img1 from './archive1_1.jpg';
+import img2 from './archive1_2.jpg';
+import img3 from './archive1_3.jpg';
+import img4 from './archive1_4.jpg';
+import img5 from './archive1_5.jpg';
+import CSNorton from 'routes/Home/Norton/CSNorton';
+
+const CSFive = (props) => {
+  const { animateIn } = props;
+
+  const content = [
+    { img: img1 },
+    { img: img2 },
+    { img: img3 },
+    { img: img4 },
+    { img: img5 }
+  ];
+
+  return (
+    <CaseStudy
+      id="archive1"
+      name="Archive 1"
+      sidebar={ true }
+      heading="In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volutpat aliquam intege."
+      content={ content }
+      next={ { id: 'norton', name: 'Norton Antivirus', component: <CSNorton /> } }
+      animateIn={ animateIn }
+    />
+  );
+};
+
+CSFive.propTypes = {
+  animateIn: React.PropTypes.bool
+};
+
+export default CSFive;
