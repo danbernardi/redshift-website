012f0c318e08483c377821bec1d258b389fe07d2
AndrewFitzpatrick <andrew@weareredshift.com>
2017-01-26 11:17:33 -0800

    bio modal adjust

--- src/routes/About/AboutTeam/BioModal.jsx
+++ src/routes/About/AboutTeam/BioModal.jsx
@@ -11,9 +11,9 @@ export function BioTemplate (props) {
   return (
     <div className={ `${bioContent.id} team-member__modal` }>
       <div className="col-12 bio--wrap row">
-        <div className="col-6 col-5--dsm col-12--tmd team-member-bio">
+        <div className="col-6 col-12--tmd team-member-bio">
           <h2 className="typ--bold">{ bioContent.name }</h2>
-          <h2 className="pb6 pb2--mlg">{ bioContent.position }</h2>
+          <h2 className="pb6 pb2--dsm">{ bioContent.position }</h2>
           { bio }
         </div>
         <div className="bio--wrap__img hide--tmd">
