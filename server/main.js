2ead09958904c3dc1af5427300ded630dde36f51
Rick Takes <richardtakes@gmail.com>
2017-02-02 11:37:16 -0800

    Updating 301 redirects

--- server/main.js
+++ server/main.js
@@ -50,12 +50,12 @@ if (project.env === 'development') {
 
   app.use((req, res, next) => {
     var host = req.get('Host');
-    if (host === 'weareredshift.com') {
-      return res.redirect(301, 'http://redshiftdigital.com' + req.originalUrl);
+    if (host === 'weareredshift.com' || host === 'www.weareredshift.com') {
+      return res.redirect(301, 'http://www.redshiftdigital.com' + req.originalUrl);
     }
 
-    if (host === 'www.redshiftdigital.com') {
-      return res.redirect(301, 'http://redshiftdigital.com' + req.originalUrl);
+    if (!host.match(/^www\..*/i)) {
+      return res.redirect(301, "http://www." + host);
     }
 
     return next();
