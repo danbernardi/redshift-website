c4accf3ca97f465f336948019edb6c666c8a363f
Daniel <danbernardi1@gmail.com>
2017-02-09 12:10:18 -0800

    disables curve editor

--- src/components/CaseStudy/CaseStudyModalWrapper.jsx
+++ src/components/CaseStudy/CaseStudyModalWrapper.jsx
@@ -3,18 +3,17 @@ import { connect } from 'react-redux';
 import { caseStudies } from 'data/caseStudies';
 import CaseStudy from './index';
 
-import MojsCurveEditor from 'mojs-curve-editor';
+// import MojsCurveEditor from 'mojs-curve-editor';
 
-const mojsCurve = new MojsCurveEditor({
-  // Name of the Curve you are working on. The name is used to
-  // identify record in `localStorage` to restore the state from
-  // when page gets reloaded, so please specify unique names if
-  // you use more than one editor on the same page.
-  name:  'bounce curve'
-});
+// const mojsCurve = new MojsCurveEditor({
+//   // Name of the Curve you are working on. The name is used to
+//   // identify record in `localStorage` to restore the state from
+//   // when page gets reloaded, so please specify unique names if
+//   // you use more than one editor on the same page.
+//   name:  'bounce curve'
+// });
 
-const easing = mojsCurve.getEasing();
-
+const easing = null; // mojsCurve.getEasing();
 
 const CaseStudyModalWrapper = props => {
   const { caseStudyState } = props;
