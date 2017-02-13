0ebc701de8e2aee818e60e28d5c1a5e40830e39e
Daniel <danbernardi1@gmail.com>
2017-02-13 15:16:51 -0800

    Adds closest number util

---
+++ src/utils/closestNumber.js
@@ -0,0 +1,12 @@
+export const getClosestNumber = (num, arr) => {
+  let curr = arr[0];
+  let diff = Math.abs(num - curr);
+  for (var val = 0; val < arr.length; val++) {
+    var newdiff = Math.abs(num - arr[val]);
+    if (newdiff < diff) {
+      diff = newdiff;
+      curr = arr[val];
+    }
+  }
+  return curr;
+};
