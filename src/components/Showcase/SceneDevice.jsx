48bf9fba7ab5e4a553568cf4f62ad2fafa64b1a3
Daniel <danbernardi1@gmail.com>
2017-02-24 16:45:31 -0800

    Adds animation hooks to scene device / text

--- src/components/Showcase/SceneDevice.jsx
+++ src/components/Showcase/SceneDevice.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import mojs from 'mo-js';
 
-export const SceneDevice = props => {
-  const { id, body, overlay, shadow } = props;
+export class SceneDevice extends React.Component {
+  componentWillReceiveProps (nextProps) {
+    const { active } = this.props;
+    if (!nextProps.active && active) {
+      // animate device out
+    }
+    if (nextProps.active && !active) {
+      // animate device in
+    }
+  }
 
-  return (
-    <div className="scene__device">
-      { body && <img className="scene__device__body" src={ body } alt={ id } /> }
-      { overlay && <img className="scene__device__overlay" src={ overlay } alt={ id } /> }
-      { shadow && <img className="scene__device__shadow" src={ shadow } alt={ id } /> }
-    </div>
-  );
-};
+  render () {
+    const { id, body, overlay, shadow } = this.props;
+
+    return (
+      <div className="scene__device">
+        { body && <img className="scene__device__body" src={ body } alt={ id } /> }
+        { overlay && <img className="scene__device__overlay" src={ overlay } alt={ id } /> }
+        { shadow && <img className="scene__device__shadow" src={ shadow } alt={ id } /> }
+      </div>
+    );
+  }
+}
 
 SceneDevice.propTypes = {
   body: React.PropTypes.string,
   overlay: React.PropTypes.string,
   shadow: React.PropTypes.string,
-  id: React.PropTypes.string
+  id: React.PropTypes.string,
+  active: React.PropTypes.bool
 };
 
 export default SceneDevice;
--- src/components/Showcase/SceneText.jsx
+++ src/components/Showcase/SceneText.jsx
@@ -2,27 +2,40 @@ import React from 'react';
 import mojs from 'mo-js';
 import { Link } from 'react-router';
 
-export const Device = props => {
-  const { caption, id } = props;
+export class Device extends React.Component {
+  componentWillReceiveProps (nextProps) {
+    const { active } = this.props;
+    if (!nextProps.active && active) {
+      // animate text out
+    }
+    if (nextProps.active && !active) {
+      // animate text in
+    }
+  }
 
-  return (
-    <div className="scene__cta typ--center typ--white">
-      <h2 className="scene__caption mb4 typ--bold">
-        { caption.map((string, index) => (
-          <span key={ index }>{ string }</span>
-        )) }
-      </h2>
+  render () {
+    const { caption, id } = this.props;
 
-      <Link className="scene__link typ--bold typ--h6" to={ `/work/${id}` }>
-        View project
-      </Link>
-    </div>
-  );
-};
+    return (
+      <div className="scene__cta typ--center typ--white">
+        <h2 className="scene__caption mb4 typ--bold">
+          { caption.map((string, index) => (
+            <span key={ index }>{ string }</span>
+          )) }
+        </h2>
+
+        <Link className="scene__link typ--bold typ--h6" to={ `/work/${id}` }>
+          View project
+        </Link>
+      </div>
+    );
+  }
+}
 
 Device.propTypes = {
   caption: React.PropTypes.array,
-  id: React.PropTypes.string
+  id: React.PropTypes.string,
+  active: React.PropTypes.bool
 };
 
 export default Device;
