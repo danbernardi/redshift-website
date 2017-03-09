b6a5ed7a5441bab7d5b813719017504242691683
AndrewFitzpatrick <andrew@weareredshift.com>
2017-03-08 16:15:33 -0800

    about page

--- src/routes/About/AboutProcess/index.jsx
+++ src/routes/About/AboutProcess/index.jsx
@@ -1,28 +1,62 @@
 import React from 'react';
 
 export function AboutProcess () {
+  const icons =[
+    {
+      img: require('assets/img/about/telescope.png'),
+      alt: 'telescope',
+      label: 'Research',
+      text: 'lorem ipsum'
+    },
+    {
+      img: require('assets/img/about/constellation.png'),
+      alt: 'constellation',
+      label: 'UX',
+      text: 'lorem ipsum'
+    },
+    {
+      img: require('assets/img/about/comet.png'),
+      alt: 'comet',
+      label: 'Visual Design',
+      text: 'lorem ipsum'
+    },
+    {
+      img: require('assets/img/about/rocket.png'),
+      alt: 'rocket',
+      label: 'Engineering',
+      text: 'lorem ipsum'
+    }
+  ];
+
   return (
     <section className="about__process mb10">
-
-      <picture>
-        <source srcSet={ require('assets/img/about/about__hero.jpg') } media="(min-width: 540px)" />
-        <img className="about__process__img mb5 mb2--msm" src={ require('assets/img/about/about__hero-mobile.jpg') } alt="Redshift team" />
-      </picture>
-      
-      <div className="row">
-        <div className="col-6 col-12--tmd">
-          <h1 className="typ--bold mb4">Hello there<span className="typ--redshift">.</span></h1>
-          <div className="typ--h4">
-            <p>Redshift is a digital experience firm.</p>
-            <p>We leverage expertise across multiple disciplines – User experience, Visual design, Engineering, and Research – to create real products and deliver experiences that are usable, beautiful, feasible and grounded in real user needs.</p>
+      <div className="col-12 row pb10">
+        <h1 className="typ--bold mb4">We are Redshift<span className="typ--redshift">.</span></h1>
+        <h3>We believe in improving people’s lives with simple, meaningful design.</h3>
+      </div>
+      <div className="col-12 theme--gray py10">
+        <div className="row">
+          <h2 className="typ--bold">Redshift is a digital experience firm that’s out of this world.</h2>
+          <h5>We leverage expertise across multiple disciplines – User experience, Visual design, Engineering, and Research – to create real products and deliver out-of-this-world experiences that are usable, beautiful, feasible and grounded in real user needs.</h5>
+          <div className="col-12 typ--center py2">
+            { icons.map((icon, i) => (
+              <span key={ i } >
+                <img
+                  src={ icon.img }
+                  alt={ icon.alt }
+                />
+              </span>
+            )) }
           </div>
         </div>
-
-        <div className="col-6 col-12--tmd col-last mt5--tmd about--process__image typ--center">
-          <img
-            src={ require('assets/img/about/process.png') }
-            alt="research engineering user experience visual design"
-          />
+        <div className="col-12">
+          <img style={ { width: '100%', position: 'absolute' } } src={ require('assets/img/about/about_curve.png') } />
+          { icons.map((icon, i) => (
+            <div className="col-3 typ--center" key={ i } style={ { padding: '11% 0' } }>
+              <h3 className="typ--bold">{ icon.label }</h3>
+              <h5>{ icon.text }</h5>
+            </div>
+          )) }
         </div>
       </div>
     </section>
