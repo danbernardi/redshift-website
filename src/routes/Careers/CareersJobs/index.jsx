0c6c9198f8fd32067ea158c6f265c8976ab35100
AndrewFitzpatrick <andrew@weareredshift.com>
2017-03-09 17:23:01 -0800

    career page start

--- src/routes/Careers/CareersJobs/index.jsx
+++ src/routes/Careers/CareersJobs/index.jsx
@@ -5,19 +5,20 @@ export function CareersJobs (props) {
   const { job } = props;
 
   const frontPage = job.frontpage.map((paragraph, index) =>
-    <p className="typ--h4 pt6 pt3--mlg" key={ index }>{ paragraph }</p>
+    <p className="typ--h4 pt3 pb2" key={ index }>{ paragraph }</p>
   );
 
   return (
-    <section className={ `${job.id} py9 py6--mlg` }>
-      <h1 className="typ--bold">{ job.position }</h1>
+    <section className={ `${job.id} py9 py6--mlg career-section` }>
+      <h2>{ job.position }</h2>
       { frontPage }
       <Link
         className="typ--h5 btn btn--arrow js-modal-trigger"
         to={ `/careers/${job.id}` }
       >
-        View position
-        <span className="icon-arrow-right pl2" />
+        <span className="job__view typ--bold">
+          View position
+        </span>
       </Link>
     </section>
   );
