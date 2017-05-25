3e4b0161e889a4b6f0b8d87d3b437f81339fb9de
AndrewFitzpatrick <andrew@weareredshift.com>
2017-05-24 17:42:13 -0700

    fullscreen video class

--- src/routes/NewAbout/AboutHero/index.jsx
+++ src/routes/NewAbout/AboutHero/index.jsx
@@ -16,21 +16,19 @@ export class AboutHero extends React.Component {
   render () {
     const { removeMask } = this.state;
     return (
-      <section className="layout--flex-col layout--fullheight" style={ { overflow: 'hidden', position: 'relative' } }>
+      <section className={ `${ removeMask ? '' : 'layout--flex-col'} layout--fullheight` } style={ { overflow: 'hidden', position: 'relative' } }>
         <div className={ `about--transition ${removeMask ? '' : 'video_mask layout--absolute layout--top'} ` } onClick={ () => this._removeMask() }>
-          <div className="video__container">
-            <video
-              autoPlay={ true }
-              loop={ true }
-              muted={ true }
-              playsInline={ true }
-              id="aboutVideo"
-              style={ { width: removeMask && '100%' } }
-            >
-              <source src="https://s3-us-west-1.amazonaws.com/rs-website-cdn/video/redshift_office_video.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
+          <video
+            autoPlay={ true }
+            loop={ true }
+            muted={ true }
+            playsInline={ true }
+            id="aboutVideo"
+            style={ { width: removeMask && '100%', transform: removeMask ? '' : 'translateX(-25%)' } }
+          >
+            <source src="https://s3-us-west-1.amazonaws.com/rs-website-cdn/video/redshift_office_video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
 
         <div className="row" style={ { zIndex: 3, display: removeMask && 'none' } }>
