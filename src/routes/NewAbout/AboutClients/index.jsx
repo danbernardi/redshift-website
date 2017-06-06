cf7e1a2198ea24757e10aaf38da40dfc46b18c19
AndrewFitzpatrick <andrew@weareredshift.com>
2017-06-06 13:04:10 -0700

    updated margins

--- src/routes/NewAbout/AboutClients/index.jsx
+++ src/routes/NewAbout/AboutClients/index.jsx
@@ -5,8 +5,8 @@ export function AboutClients (props) {
   const { data } = props;
 
   return (
-    <section className="row layout--flex my8">
-      <div className="row typ--center">
+    <section className="row layout--flex">
+      <div className="row typ--center my8">
         <h1 className="typ--bold typ--redshift mb6 mb4--mlg">Who we work with.</h1>
         <div className="clients">
           { data.map((client, index) => (
