beae64df2df97b2d277d47ddff3f5af48bfd17c1
Daniel <danbernardi1@gmail.com>
2017-06-23 16:20:57 -0700

    Adds character build animations to casestudy headers

---
+++ src/components/CaseStudy/CaseStudyHeader.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TimelineMax, Power3 } from 'gsap';
+import SplitText from 'vendor/gsap-plugins/SplitText';
+import GSAP from 'react-gsap-enhancer';
+
+export class CaseStudyHeader extends React.Component {
+  componentDidMount () {
+    const textChars = new SplitText('.casestudy__title', { type: 'chars, words' });
+
+    setTimeout(() => {
+      this.animation = this.addAnimation(this.animateInHeader.bind(this), { text: textChars }).play();
+    }, 600);
+  }
+
+  animateInHeader ({ target, options }) {
+
+    return new TimelineMax({ target, options })
+      .set('.casestudy__title', { perspective: 400 })
+      .staggerTo(options.text.chars, 0.2, {
+        opacity: 1,
+        y: 0,
+        ease: Power3.easeOut
+      }, 0.02);
+  }
+
+  render () {
+    const { classes, content } = this.props;
+
+    return (
+      <h1 className={ `${classes} typ--bold casestudy__title` }>
+        { content }
+      </h1>
+    );
+  }
+}
+
+export default (GSAP()(CaseStudyHeader));
