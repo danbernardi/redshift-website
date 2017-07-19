d63fc3e4e0e52f4ca1cc4e143dab81e6826bbcac
Daniel <danbernardi1@gmail.com>
2017-07-19 14:06:23 -0700

    Tweaks archive grid styling

--- src/components/Archive/index.jsx
+++ src/components/Archive/index.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import { caseStudies } from 'data/caseStudies';
+import Isotope from 'isotope-layout';
+import 'masonry-layout';
 import './Archive.scss';
 
-export function Archive () {
-  return (
-    <section className="archive row">
-      <h1 className="typ--bold typ--center mb10">Selected work.</h1>
+export class Archive extends React.Component {
+  componentDidMount () {
+    setTimeout(() => {
+      this.isotope = new Isotope(this.grid, {
+        itemSelector: '.archive__item',
+        layoutMode: 'masonry'
+      });
+    }, 200);
+  }
 
-      <div className="archive__grid">
-        { caseStudies.map((study, index) => (
-          <div className="archive__item theme--dark" key={ index }>
-            { study.gridThumbnail && <img src={ study.gridThumbnail } alt={ study.name } /> }
-            <h3 className="archive__name typ--bold">{ `${ study.name }.` }</h3>
-            <div className="archive__overlay">
-              <h2 className="typ--bold">{ study.shortDescription }</h2>
+  render () {
+    return (
+      <section className="archive row">
+        <h1 className="typ--bold typ--center mb10">Selected work.</h1>
+
+        <div className="archive__grid" ref={ el => { this.grid = el; } }>
+          { caseStudies.map((study, index) => (
+            <div className="archive__item theme--dark" key={ index }>
+              { study.gridThumbnail ? <img src={ study.gridThumbnail } alt={ study.name } /> : <div className="archive__imgspacer" /> }
+              <h4 className="archive__name typ--bold">{ `${ study.name }.` }</h4>
+              <div className="archive__overlay">
+                <h2 className="typ--bold">{ study.shortDescription }</h2>
+              </div>
             </div>
-          </div>
-        )) }
-      </div>
-    </section>
-  );
+          )) }
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Archive;
