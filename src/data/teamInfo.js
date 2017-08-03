9744f914d56bf5812a0125e2d33ce3e8d76c2ed8
AndrewFitzpatrick <andrew@weareredshift.com>
2017-08-03 11:28:33 -0700

    about page update

--- src/data/teamInfo.js
+++ src/data/teamInfo.js
@@ -3,6 +3,7 @@ export const teamInfo = [
     id: 'david',
     name: 'David Westen',
     position: 'Principal & Founder',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/david.jpg',
     bioStatement: [
       'David is an expert in internet technology, user experience and digital business strategy.',
       'In 1999 David co-founded Internet Learning Corporation (ILC), a VC-backed startup focusing on creative, customized e-learning solutions, which was acquired in 2002 by A.S.K. Learning. During his seven years as CTO of A.S.K., David led the development of e-learning initiatives for clients including HP, Cisco Systems, Veritas, EMC, Sony, Commonwealth Bank and PriceWaterHouseCoopers.',
@@ -13,6 +14,7 @@ export const teamInfo = [
     id: 'steffan',
     name: 'Steffan Schlarb',
     position: 'Creative Director',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/steffan.jpg',
     bioStatement: [
       'Steffan is an award-winning creative director with extensive innovation, creative strategy and product design experience.',
       'He has helped shape the creative vision of REDSHIFT for the past 5 years. He leads a team of designers in creating innovative and engaging digital products.',
@@ -24,6 +26,7 @@ export const teamInfo = [
     id: 'stewart',
     name: 'Stewart Maclennan',
     position: 'Director of Digital Media',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/stewart.jpg',
     bioStatement: [
       'Stewart Maclennan is a new media guru who combines 12 years of web consulting experience with 9 years as a film writer, director and producer.',
       'Stewart\'s work includes film production for theatrical and web distribution, interactive internet campaigns, and design and branding solutions. Drawing on his dual interests, Stewart has worked with companies such as Hewlett-Packard and Symantec to leverage new media strategies to accomplish their business objectives.',
@@ -34,6 +37,7 @@ export const teamInfo = [
     id: 'scott',
     name: 'Scott Lambridis',
     position: 'Lead UX Director',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/scott.jpg',
     bioStatement: [
       'Scott Lambridis is the director of UX at Redshift.',
       'He grew up in New York and earned a degree in neurobiology from the University of Virginia. After designing and coding custom educational tools for large and mid-sized companies, Scott built a UX department at Eveo and designed numerous mobile and desktop applications for doctors, patients, and healthcare reps.',
@@ -44,6 +48,7 @@ export const teamInfo = [
     id: 'lucy',
     name: 'Lucy Dotson',
     position: 'Senior UX Designer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/lucy.jpg',
     bioStatement: [
       'Lucy Dotson is a senior user experience designer at Redshift.',
       'After receiving her degree in English from Colby College, Lucy crafted engaging stories for companies of all sizes as a copywriter and content strategist. Lucy discovered her appreciation for design while working as a ux writer, and shortly after decided to pursue ux full time. Since then, Lucy has designed experiences for clients including Kaiser Permantente, Western Union, and Advanced Microgrid Solutions (AMS).',
@@ -54,6 +59,7 @@ export const teamInfo = [
     id: 'kyle',
     name: 'Kyle Lloyd',
     position: 'Director of Business Development',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/kyle.jpg',
     bioStatement: [
       'Kyle grew up in Austin, TX and went to school at Texas State University in San Marcos where he earned a BBA in Marketing. He prides himself in building successful sales organizations from the ground up, and prior to Redshift, Kyle worked for pharmaceutical companies, technology accelerators, and other world-renowned digital design firms.',
       'A proud Texan (in name only), Kyle indefinitely resides in San Francisco, CA with his longtime partner. You can find him any given weekend on hiking trails around the Bay Area.'
@@ -63,6 +69,7 @@ export const teamInfo = [
     id: 'shannon',
     name: 'Shannon Chin',
     position: 'Senior Interactive Producer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/shannon.jpg',
     bioStatement: [
       'Shannon Chin is an interactive producer at Redshift.',
       'She is born and raised in San Francisco, CA and attended Boston University for her BS in Business. Prior to working at Redshift, Shannon has worked on digital marketing and advertising campaigns and projects for clients including Google, Nike, Visa and Levi\'s.',
@@ -73,6 +80,7 @@ export const teamInfo = [
     id: 'ian',
     name: 'Ian Eck',
     position: 'UX Designer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/ian.jpg',
     bioStatement: [
       'Ian is a user experience designer at Redshift.',
       'He grew up outside of Philadelphia, PA and studied at Wake Forest University—immersing himself in English, Economics, and the charms of southern hospitality. After school he moved to San Francisco, where he cut his journalistic teeth at San Francisco Magazine. He also blogged for various nifty tech ventures and learned about UX design through General Assembly.',
@@ -83,16 +91,18 @@ export const teamInfo = [
     id: 'andrew',
     name: 'Andrew Fitzpatrick',
     position: 'Software Engineer',
+    modal: require('assets/img/about/team/modal/andrew_2.jpg'),
     bioStatement: [
       'Andrew is a self-taught developer with a background in fine arts.',
-      'He has built websites for some of the largest (and smallest) online retailers in the world over the last decade. When he is not writing the code that makes your page load as a Full Stack Engineer at Redshift, you can find him lurking in thrift shops and garage sales for inspiration for his artwork and products. His products have been sold in various quirky boutiques worldwide.',
-      'Originally from Philadelphia, via Austin and Los Angeles, he currently lives in Alameda with his wife Ellen and his crabby dog, Ajax Pupperknuckles.'
-    ]
+      'He has built websites for the largest online retailers over the last decade. When he is not writing the code that makes your page load as a Full Stack Engineer at Redshift, you can find him lurking in thrift shops and garage sales.'
+    ],
+    inspiration: true
   },
   {
     id: 'sasha',
     name: 'Sasha Klein',
     position: 'Senior Software Engineer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/sasha.jpg',
     bioStatement: [
       'Sasha Klein is a senior web developer at Redshift.',
       'He grew up in Washington, DC, got a BA in English from Harvard College, taught in China, and then took a sharp turn into web development upon arriving in SF. Before landing at Redshift, Sasha coded and ran the web dev curriculum at Bloc, an online tech boot camp. He also gave startups a shot, founding and then giving up on a meat-sharing and a social-travel startup, both of which he requires alcohol to discuss.',
@@ -103,6 +113,7 @@ export const teamInfo = [
     id: 'emma',
     name: 'Emma Hazlett',
     position: 'Visual Designer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/emma.jpg',
     bioStatement: [
       'Emma Hazlett is a visual designer at Redshift.',
       'She grew up in Norwich, VT. She attended the University of Vermont for Anthropology and pre-med. She received her graphic design training at MICA.',
@@ -113,6 +124,7 @@ export const teamInfo = [
     id: 'sandra',
     name: 'Sandra Wing',
     position: 'UX Researcher',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/sandra.jpg',
     bioStatement: [
       'Sandra Wing is a User Experience Researcher at Redshift.',
       'Sandra is a native of San Francisco, CA and received a B.A. in Global Studies with a minor in Asian Languages from UCLA. Influenced by her upbringing in San Francisco’s diverse neighborhoods, she was always interested in understanding what motivates people and why people do the things they do. Before working at Redshift, she worked as a Research and GIS Analyst for UCLA’s North American Integration and Development Center.',
@@ -123,6 +135,7 @@ export const teamInfo = [
     id: 'trevor',
     name: 'Trevor Alexander',
     position: 'Software Engineer',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/trevor.jpg',
     bioStatement: [
       'Trevor Alexander is a software engineer and data analyst at Redshift. Raised in Arizona, he received his B.S. in Electrical Engineering from the University of Hawai`i at Manoa, with a focus in systems engineering and a minor in Japanese. Following that, he received an M.S. in Electrical Engineering from the same, with a thesis in the area of brain-computer interfaces. Prior to working at Redshift, he worked for a number of technical startups, developing a passion for application development and high-performance web engineering.',
       'He lives in Alameda, CA with his cat, Dan.'
@@ -132,6 +145,7 @@ export const teamInfo = [
     id: 'diana',
     name: 'Diana Cheng',
     position: 'Director of User Research',
+    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/diana.jpg',
     bioStatement: [
       'Diana Cheng is the Director of User Research at Redshift.',
       'She has a BA from UC Berkeley in Political Science and Art. She received her Masters from the IIT Institute of Design in Chicago, which teaches ethnography and other social science methods as part of the human-centered design process. Prior to Redshift, she held design, strategy and innovation roles for such companies as Google, Panasonic, and Jump Associates. She is a regular contributor to the Design Thinking program at the Rochester Institute of Technology.',
@@ -142,6 +156,7 @@ export const teamInfo = [
     id: 'mattk',
     name: 'Matt Kissick',
     position: 'UX Designer',
+        modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/mattk.jpg',
     bioStatement: [
       'Matt Kissick is a user experience designer at Redshift.',
       'He grew up in Ottawa, Canada and went to OCAD University in Toronto. He received a Bachelor of Design in Industrial Design. Prior to Redshift, Matt freelanced as a graphic and web designer.',
@@ -152,6 +167,7 @@ export const teamInfo = [
     id: 'mattr',
     name: 'Matt Rancatore',
     position: 'Visual Designer',
+        modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/mattr.jpg',
     bioStatement: [
       'Matt Rancatore is a visual designer at Redshift.',
       'He grew up in the Bay Area and received a BS in Graphic Design from The Art Institute of California – San Francisco. Prior to working at Redshift, Matt designed magazines, brands, and websites at companies including Wired Magazine, Sidecar Agency, and IDEO. When Matt isn\'t hunched over his computer pushing pixels, he can be found biking up San Francisco\'s notorious hills, in a park napping, or at a baseball game.'
