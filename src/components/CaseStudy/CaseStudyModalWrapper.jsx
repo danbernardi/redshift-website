151976ecd38b9d3d3594cb7f60839e39cfc38efd
Daniel <danbernardi1@gmail.com>
2017-08-03 10:35:31 -0700

    Removes reference to mojs

--- src/components/CaseStudy/CaseStudyModalWrapper.jsx
+++ src/components/CaseStudy/CaseStudyModalWrapper.jsx
@@ -5,18 +5,6 @@ import CaseStudy from './index';
 import TransitionGroup from 'react-addons-transition-group';
 import PropTypes from 'prop-types';
 
-// import MojsCurveEditor from 'mojs-curve-editor';
-
-// const mojsCurve = new MojsCurveEditor({
-//   // Name of the Curve you are working on. The name is used to
-//   // identify record in `localStorage` to restore the state from
-//   // when page gets reloaded, so please specify unique names if
-//   // you use more than one editor on the same page.
-//   name:  'bounce curve'
-// });
-
-const easing = null; // mojsCurve.getEasing();
-
 const CaseStudyModalWrapper = props => {
   const { caseStudyState, caseContent } = props;
   const caseStudyComponents = caseStudies.map((cs, index) => ({
@@ -25,7 +13,6 @@ const CaseStudyModalWrapper = props => {
       <CaseStudy
         { ...cs }
         key={ index }
-        easing={ easing }
         caseStudyState={ caseStudyState }
         caseStudyContent={ caseContent }
       />
