f06747c458ad925548f3060f02c9bfb60db47057
AndrewFitzpatrick <andrew@weareredshift.com>
2017-08-08 12:14:10 -0700

    right arrow archive

--- src/components/Archive/index.jsx
+++ src/components/Archive/index.jsx
@@ -70,7 +70,7 @@ export class Archive extends React.Component {
                     }
                     <h4 className="archive__name typ--bold">{ `${ study.name }.` }</h4>
                     <div className="archive__overlay" style={ { backgroundColor: study.color } }>
-                      <div className="p2">
+                      <div className="archive__overlay--text">
                         <h3 className="typ--bold">{ study.shortDescription }</h3>
                       </div>
                       <div className="archive__arrow">
