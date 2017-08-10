45c1268467edc7798d4d923011a493c5f2b7665d
Sasha Klein <sashafklein@gmail.com>
2017-08-10 16:45:26 -0700

    Switched to SVGs and normalized widths

--- src/data/newClients.js
+++ src/data/newClients.js
@@ -1,14 +1,14 @@
 export const clientData = [
-  { image: require('assets/img/about/clients/Google.png'), name: 'google' },
-  { image: require('assets/img/about/clients/Panasonic.png'), name: 'panasonic' },
-  { image: require('assets/img/about/clients/OpenTable.png'), name: 'Open Table' },
-  { image: require('assets/img/about/clients/Kaiser.png'), name: 'Kaiser Permanente' },
-  { image: require('assets/img/about/clients/WesternUnion.png'), name: 'Western Union' },
-  { image: require('assets/img/about/clients/TaskRabbit.png'), name: 'Task Rabbit' },
-  { image: require('assets/img/about/clients/Facebook.png'), name: 'Facebook' },
-  { image: require('assets/img/about/clients/JDPower.png'), name: 'JD Power' },
-  { image: require('assets/img/about/clients/MyPoints.png'), name: 'MyPoints' },
-  { image: require('assets/img/about/clients/Symantec.png'), name: 'Symantec' },
-  { image: require('assets/img/about/clients/Groupon.png'), name: 'Groupon' },
-  { image: require('assets/img/about/clients/Vlocity.png'), name: 'Vlocity' }
+  { image: require('assets/img/about/clients/Google.svg'), name: 'google' },
+  { image: require('assets/img/about/clients/Panasonic.svg'), name: 'panasonic' },
+  { image: require('assets/img/about/clients/OpenTable.svg'), name: 'Open Table' },
+  { image: require('assets/img/about/clients/Kaiser.svg'), name: 'Kaiser Permanente' },
+  { image: require('assets/img/about/clients/WesternUnion.svg'), name: 'Western Union' },
+  { image: require('assets/img/about/clients/TaskRabbit.svg'), name: 'Task Rabbit' },
+  { image: require('assets/img/about/clients/Facebook.svg'), name: 'Facebook' },
+  { image: require('assets/img/about/clients/JDPower.svg'), name: 'JD Power' },
+  { image: require('assets/img/about/clients/MyPoints.svg'), name: 'MyPoints' },
+  { image: require('assets/img/about/clients/Symantec.svg'), name: 'Symantec' },
+  { image: require('assets/img/about/clients/Groupon.svg'), name: 'Groupon' },
+  { image: require('assets/img/about/clients/Vlocity.svg'), name: 'Vlocity' }
 ];
