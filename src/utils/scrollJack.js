6580bba338feaa129abdfcc25da908dedd29ff4e
Daniel <danbernardi1@gmail.com>
2017-08-10 12:05:59 -0700

    Simplifies scrollJack

--- src/utils/scrollJack.js
+++ src/utils/scrollJack.js
@@ -10,22 +10,14 @@ export function getScrollDirection (previousYPosition, currentYPosition) {
   return null;
 }
 
-let scrollPos;
-
  // enables free page scrolling
 export function enableScroll () {
   const html = document.getElementsByTagName('html');
-  const scroller = document.scrollingElement;
   html[0].classList.remove('disable-scroll');
-  debugger;
-  scroller.scrollTop = scrollPos;
 }
 
 // disables free page scrolling
 export function disableScroll () {
   const html = document.getElementsByTagName('html');
-  console.log(html);
-  const scroller = document.scrollingElement;
-  scrollPos = scroller.scrollTop;
   html[0].classList.add('disable-scroll');
 }
