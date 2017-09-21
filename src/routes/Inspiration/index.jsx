8f8aa334a32ed811f14e1f4af9107bb22e821add
Rick Takes <richardtakes@gmail.com>
2017-09-21 11:39:22 -0700

    Readding prop type to inspiration

--- src/routes/Inspiration/index.jsx
+++ src/routes/Inspiration/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from 'components/Footer';
 import { ellipsisString } from 'utils/string';
 import { TimelineLite, Power2 } from 'gsap';
