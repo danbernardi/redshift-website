36ad4003d4c11c6d009198aa390604e9ca4e083d
AndrewFitzpatrick <andrew@weareredshift.com>
2017-10-05 20:25:37 -0700

    tablet m about page

--- src/data/newClients.js
+++ src/data/newClients.js
@@ -1,14 +1,14 @@
 export const clientData = [
   { image: require('assets/img/about/clients/Google.svg'), name: 'google' },
   { image: require('assets/img/about/clients/Panasonic.svg'), name: 'panasonic' },
-  { image: require('assets/img/about/clients/OpenTable.svg'), name: 'Open Table' },
   { image: require('assets/img/about/clients/Kaiser.svg'), name: 'Kaiser Permanente' },
   { image: require('assets/img/about/clients/WesternUnion.svg'), name: 'Western Union' },
-  { image: require('assets/img/about/clients/TaskRabbit.svg'), name: 'Task Rabbit' },
-  { image: require('assets/img/about/clients/Facebook.svg'), name: 'Facebook' },
   { image: require('assets/img/about/clients/JDPower.svg'), name: 'JD Power' },
   { image: require('assets/img/about/clients/MyPoints.svg'), name: 'MyPoints' },
+  { image: require('assets/img/about/clients/Facebook.svg'), name: 'Facebook' },
   { image: require('assets/img/about/clients/Symantec.svg'), name: 'Symantec' },
+  { image: require('assets/img/about/clients/OpenTable.svg'), name: 'Open Table' },
   { image: require('assets/img/about/clients/Groupon.svg'), name: 'Groupon' },
+  { image: require('assets/img/about/clients/TaskRabbit.svg'), name: 'Task Rabbit' },
   { image: require('assets/img/about/clients/Vlocity.svg'), name: 'Vlocity' }
 ];
