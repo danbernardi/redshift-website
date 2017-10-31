62c743d017cb5b5dc84e558ad0b5cd826dd57271
Daniel <danbernardi1@gmail.com>
2017-10-31 12:10:53 -0700

    Adds casse study color to hamburger

--- src/components/Hamburger/index.jsx
+++ src/components/Hamburger/index.jsx
@@ -10,26 +10,33 @@ import './Hamburger.scss';
   * @param {Object} props
   * @param {Object} modalState          returns information about current modal
   * @param {boolean} close              close modal
+  * @param {string} color               String defining color of the hamburger spans
   * @returns {func}                     Returns a function
 */
 
 export function Hamburger (props) {
-  const { modalState, close } = props;
+  const { modalState, close, color } = props;
+  const spanStyles = {};
+
+  if (color) {
+    spanStyles.backgroundColor = color;
+  }
 
   return (
     <a
       href="javascript:void(0)"
       className={ `icon-hamburger ${(modalState.open && modalState.modalID === 'nav' || close) && 'close-icon'}` }
     >
-      <span />
-      <span />
+      <span style={ spanStyles } />
+      <span style={ spanStyles } />
     </a>
   );
 };
 
 Hamburger.propTypes = {
   modalState: PropTypes.object,
-  close: PropTypes.bool
+  close: PropTypes.bool,
+  color: PropTypes.string
 };
 
 Hamburger.defaultProps = {
