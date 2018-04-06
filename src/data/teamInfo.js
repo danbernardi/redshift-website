da2e864ed86b8b5a63df6e8b5a68cff8f2414fcb
Rick Takes <richardtakes@gmail.com>
2018-04-06 15:36:50 -0700

    Adding new team members

--- src/data/teamInfo.js
+++ src/data/teamInfo.js
@@ -89,17 +89,6 @@ export const teamInfo = [
     ]
   },
   {
-    id: 'sasha',
-    name: 'Sasha Klein',
-    position: 'Senior Software Engineer',
-    modal: 'https://s3-us-west-1.amazonaws.com/rs-website-cdn/images/about/team/modal/sasha.jpg',
-    bioStatement: [
-      'Sasha Klein is a senior web developer at Redshift.',
-      'He grew up in Washington, DC, got a BA in English from Harvard College, taught in China, and then took a sharp turn into web development upon arriving in SF. Before landing at Redshift, Sasha coded and ran the web dev curriculum at Bloc, an online tech boot camp. He also gave startups a shot, founding and then giving up on a meat-sharing and a social-travel startup, both of which he requires alcohol to discuss.',
-      'When Sasha isn\'t complimenting coworkers on their lunches, he likes to trap them in discussions about universal basic income.'
-    ]
-  },
-  {
     id: 'danielle',
     name: 'Danielle Raynal',
     position: 'UX Researcher',
@@ -162,6 +151,113 @@ export const teamInfo = [
     bioStatement: [
       'Matt Rancatore is a visual designer at Redshift.',
       'He grew up in the Bay Area and received a BS in Graphic Design from The Art Institute of California – San Francisco. Prior to working at Redshift, Matt designed magazines, brands, and websites at companies including Wired Magazine, Sidecar Agency, and IDEO. When Matt isn\'t hunched over his computer pushing pixels, he can be found biking up San Francisco\'s notorious hills, in a park napping, or at a baseball game.'
+    ]
+  },
+
+
+  {
+    id: 'andrewL',
+    name: 'Andrew Lenzini',
+    modal: '',
+    bioStatement: [
+      'Andrew Lenzini is a Visual Designer at Redshift.',
+      'He grew up in San Marcos, CA and received his BFA in Graphic Design & Marketing with a minor in Computer Science from California State University, San Bernardino.',
+      'Andrew has helped design products for companies such as Visa, Samsung, and T-Mobile.',
+      'He now lives in Walnut Creek, CA with his boyfriend and an Instagram-famous cat. Meow!'
+    ]
+  },
+  {
+    id: 'chrisM',
+    name: 'Chris Mueller',
+    modal: '',
+    bioStatement: [
+     'Chris Mueller is a UX designer at Redshift.',
+     'He grew up in Scottsdale, AZ and studied Computer Science at LMU in Los Angeles. More interested in making cool stuff than analyzing algorithms, he pivoted to design with a master\'s in HCI from Carnegie Mellon. Before Redshift, Chris was leading design and research projects at NASA.',
+     'Outside of work, you can find Chris at a show, in the woodshop, or getting his heart broken by the Arizona Cardinals.'
+    ]
+  },
+  {
+    id: 'brock',
+    name: 'Brock Lanoza',
+    modal: '',
+    bioStatement: [
+    'Brock Lanoza is a Software Engineer at Redshift.',
+    'A New Jersey native, Brock moved to attend the University of Hawaii where he first discovered his interest in building software. Before joining the Redshift team, Brock helped augment enterprise teams, teaching them how to build performant and scalable full-stack applications.',
+    'When not pondering the finer points of human-computer interaction, Brock can be found shamelessly spam-posting pictures of Persian cats to the internet.'
+    ]
+  },
+  {
+    id: 'davidN',
+    name: 'David Noah',
+    modal: '',
+    bioStatement: [
+      'David Noah is a Software Engineer at Redshift.',
+      'He grew up in Frederick, Maryland and earned a degree in geographic information systems from the University of Maryland. The technical nature of computer cartography was a catalyst for David’s pivot into web development. He quickly joined RapidAPI to help build the world largest API marketplace.',
+      'David spends his weekends exploring local live music, adventuring outdoors, and brewing beer with friends.'
+    ]
+  },
+  {
+    id: 'jason',
+    name: 'Jason Murphy',
+    modal: '',
+    bioStatement: [
+      'Jason is the managing director at Redshift.',
+      'Jason has a product management background  and has built and launched three products since 2012 across multiple verticals.  Jason most recently ran product for Panasonic\'s IoT and Analytics group where he and his team launched Smart Cities for the city of Denver.',
+      'A life-long learner, Jason\'s academic pursuits include University of Texas-Austin, Pepperdine University, Stanford University and Imperial College, London.',
+      'When not looking after Redshift\'s bottom line, Jason can be found with his audible books or TED talks.'
+    ]
+  },
+  {
+    id: 'benH',
+    name: 'Ben Hester',
+    modal: '',
+    bioStatement: [
+      'Ben Hester is an associate director of UX at Redshift.',
+      'Ben’s work blends his background in business strategy and user-centered design, helping organizations large and small succeed by focusing on the needs of the people they serve. Prior to joining Redshift Ben led the UX and client services teams at Free Range Studios, obtained an MBA at the Presidio School of Management, and led design teams at a variety of organizations.',
+      'When he’s not leading design teams and prototyping he can be found hiking the East Bay hills with his family, grilling on his deck with friends, and cheering for the Oakland A’s.'
+    ]
+  },
+  {
+    id: 'ally',
+    name: 'Ally Lucas',
+    modal: '',
+    bioStatement: [
+      'Ally is the studio manager at Redshift.',
+      'Ally grew up in Sonoma County and earned a degree in English from CSU Sacramento. Working with startups, she has assisted with developing products through QA, data analysis, and application support.',
+      'When not at Redshift, Ally can be found at a show, hunting for antique furniture, or chasing her dog Larry through Golden Gate Park.'
+    ]
+  },
+  {
+    id: 'caryn',
+    name: 'Caryn Arredondo',
+    modal: '',
+    bioStatement: [
+      'Caryn Arredondo is a user experience designer at Redshift.',
+      'She grew up in Mexico and moved to the United States to study',
+      'Visual Design. Prior to working at Redshift, she worked in advertising and design for various firms in Denver and London. After arriving in San Francisco, she added UX/UI design to her design palette.',
+      'A traveler at heart, she now lives and hikes in the Redwood Hills of Oakland, CA, with her freckly Aussie dog Mishka.'
+    ]
+  },
+  {
+    id: 'juliana',
+    name: 'Juliana Peters',
+    modal: '',
+    bioStatement: [
+      'Juliana Peters is a User Experience Researcher at Redshift.',
+      'Juliana spends her time interpreting human behavior and breathing life into data. With 10+ years of research experience under her belt, her expertise lies in translating user needs into product strategy and design.',
+      'Prior to working at Redshift, Juliana has led end-to-end research projects for clients such as Google, Instagram, The Hershey Company and Guggenheim.',
+      'She grew up in Florida and has a B.S. in Psychology and a B.A. in Spanish from the University of Florida. She received her Master’s in Cognitive Psychology from Penn State University.',
+      'Juliana has a penchant for all things citrus, and in her free time loves making jam, Facetiming with her nieces and nephews, watching Florida Gator sports, and playing board games.'
+    ]
+  },
+  {
+    id: 'chrisF',
+    name: 'Chris Fairchild',
+    modal: '',
+    bioStatement: [
+      'Chris Fairchild is a visual designer and type nerd at Redshift.',
+      'He grew up in a tiny mountain town in NE GA (cue banjo) before receiving a BA in Mass Communications at University of Georgia. He then attended the Creative Circus before jetting cross country to the Bay Area. Since then, Chris has designed and art directed ad content for Skype, user experiences for Audi and branding projects for Dolby, HTC and a slew of startups.',
+      'When he’s not designing for Redshift, he keeps himself sharp with his own design projects. Chris also likes getting outdoors as much as possible with his German shepherd sidekick Goose (yes, it’s a Top Gun reference).'
     ]
   }
 ];
